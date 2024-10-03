037ad760fd6f6f4de8c167d7661777ed571e9554
noppiii <mnoviantoanggoro@gmail.com>
2024-10-03 11:59:50 +0700

    login page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/login', [LoginController::class, 'index'])->name('login');
 
 Route::middleware([
     'auth:sanctum',
